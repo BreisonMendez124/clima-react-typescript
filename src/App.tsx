d9e7157b636d25ba062149148094d48b41c4812d
Breison Mendez <breisonmendez78@gmail.com>
2024-05-20 19:19:16 -0500

    Inicializacion de proyecto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <> 
+
+      <h1 className={styles.title}>buscador de clima</h1>
+      <div className={ styles.container }>
+          <p>1</p>
+          <p>2</p>
+      </div>
+    </>
+  )
+}
+
+export default App
