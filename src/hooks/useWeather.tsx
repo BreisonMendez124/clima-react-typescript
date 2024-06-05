6da4005f89804ab9ac50423b92b6ec538b3731d1
Breison Mendez <breisonmendez78@gmail.com>
2024-06-04 20:04:58 -0500

    Tipar respuesta de API con ZOD o Valibot

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,5 +1,30 @@
 import axios from 'axios';
 import { SearchType } from '../types';
+import { string , object , number , InferInput , parse } from 'valibot'
+// import { z } from 'zod'
+
+// Zod
+// const Weather = z.object({
+//     name: z.string(),
+//     main: z.object({ 
+//         temp: z.number(),
+//         temp_max: z.number(),
+//         temp_min: z.number()
+//     })
+// })
+// type Weather = z.infer< typeof Weather >
+
+//Valibot
+
+const WeatherSchema = object({
+    name: string(),
+    main: object({ 
+        temp: number(),
+        temp_max: number(),
+        temp_min: number()
+    })
+})
+type Weather = InferInput< typeof WeatherSchema >
 
 export default function useWeather(){
     
@@ -7,13 +32,28 @@ export default function useWeather(){
         const appId = import.meta.env.VITE_API_KEY;
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${ search.country}&appid=${appId}`;
-            console.log(geoUrl)
             console.log('Consultando....')
-            const { data } = await axios( geoUrl )
-            console.log(data)
+            const { data } = await axios.get( geoUrl )
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            const wheatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            const { data: weatherResult } = await axios.get( wheatherUrl );
+            //ZOD
+            // const result = Weather.safeParse( weatherResult )
+            // if( result.success ){
+            //     console.log( result.data.name )
+            //     console.log( result.data.main.temp )
+            // }
+            //Valibot
+            const result = parse( WeatherSchema,  weatherResult );
+            console.log( result )
+            if( result ){
+                console.log( result.name )
+            }
+
             
         } catch (error) {
-            
+            console.log( error )
         }
     }
     return { 
